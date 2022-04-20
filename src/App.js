04db2c4708265afccb523adb9b2f139399a1d6ac
ChloeBezault <chloe.b@outlook.pt>
2022-04-20 18:46:13 +0100

    Adicionado ficheiro images

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+import Create from "./components/Create";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
+  const [stocks, setStocks] = useState([]);
+
+  useEffect(() => {
+    fetch("https://justivo.com/stockws.php?get")
+      .then((response) => response.json())
+      .then((dataReceived) => setStocks(dataReceived));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home stocks={stocks} />} />
+          <Route path="/create" element={<Create />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
